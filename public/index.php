3e24d638473bc5650662c8bb6c6c8a11380a188e
Rudy <kanachou@pop-os.localdomain>
2021-04-12 12:12:39 +0200

    made composer quest 5.2

--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 require __DIR__ . "/../vendor/autoload.php";
+
+
+use HelloWorld\SayHello;
+
+echo SayHello::world();
 // require __DIR__ . "/../src/Wcs/Hello.php";
-$sayhello = new App\Wcs\Hello();
-echo $sayhello->talk();
+// $sayhello = new App\Wcs\Hello();
+// echo $sayhello->talk();
